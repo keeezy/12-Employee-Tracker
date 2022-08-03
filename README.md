4efdcc73623f9e540803a7ea871e76f24a648595
keeezy <lazarokev@gmail.com>
2022-08-02 17:08:28 -0700

    cleaned up old code, updated readMe

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# 12-Employee-Tracker
-SQL
+# Employee Tracker
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+Achitect and build a solution for managing a company's employees using node, inquirer, and MySQL.
+
+## Table of Contents:
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+* Load  the schema.sql and seeds.sql to sql database
+* Download and clone the repository to your machine
+* In the CLI, enter these commands below
+    * `npm i` to install dependencies
+    * `node index.js` to begin the CLI
+
+## Usage:
+<br>Application Preview
+
+* Node.js
+* Express.js
+* MongoDB
+* Mongoose ODM
+
+
+## License:
+This Project is licensed under: MIT License
+
+## Contributing:
+Only me so far
+
+## Tests:
+No tests at the moment
+
+## Questions:
+If there are questions you can reach me via github/email. Listed below
+- https://github.com/keeezy
+- lazarokev@gmail.com 
--- index.js
+++ index.js
@@ -24,124 +24,6 @@ const startQuestions = {
     ]
 }
 
-// const addEmployeeQuestions = [
-//     {
-//         name: "first_name",
-//         message: "What is the employee's first name?",
-//     },
-//     {
-//         name: "last_name",
-//         message: "What is the employee's last name?"
-//     },
-//     {
-//         name: "role_id",
-//         message: "What is the employee's title?",
-//         type: "list",
-//         choices: [
-//             {}
-//         ]
-//     },
-//     {
-//         name: "manager_id",
-//         message: "Who is this employee's manager?",
-//         type: "list",
-//     }
-
-// ]
-
-// const addEmployee = () => {
-//     // Need to inquirer to gather info on new employee
-//     // we need all the currert rol id to allow choose role_id thats in role table,
-//     // we need all the current emp ids, to choose manager_id
-//     db.query(`SELECT id, first_name, last_name FROM employee`
-//     ).then((managers) => {
-
-//         console.log("managers-----------", managers)
-//         const managerChoices = managers.map(man => {
-//             return {
-//                 name: `${man.first_name} ${man.last_name}`,
-//                 value: man.id
-//             }
-//         })
-//         db.query(`SELECT id, title FROM role`).then((results) => {
-//             console.table(results);
-//             const choices = results.map((role) => {
-//                 return {
-//                     name: role.title,
-//                     value: role.id
-//                 }
-//             });
-
-//             console.log("CHOICES MADE FOR INQUIRER PROMPT----", choices)
-//             // converts results to an array of choices for prompt
-//             const addEmployeeQuestions = [
-//                 {
-//                     name: "first_name",
-//                     message: "What is the employee's first name?",
-//                 },
-//                 {
-//                     name: "last_name",
-//                     message: "What is the employee's last name?"
-//                 },
-//                 {
-//                     name: "role_id",
-//                     message: "What is the employee's title?",
-//                     type: "list",
-//                     choices
-//                 },
-//                 {
-//                     name: "manager_id",
-//                     message: "Who is this employee's manager?",
-//                     type: "list",
-//                     choices: managerChoices
-//                 }
-//             ];
-
-//             inquirer.prompt(addEmployeeQuestions)
-//                 .then(results => {
-//                     console.log("results -----", results)
-//                 db.query("INSERT INTO employee SET ?", results)
-//                 .then((start))
-//                 });
-//         });
-//     });
-// }
-
-// const showAllEmployees = () => {
-//     // calls to db, and show all employees
-//     db.query("SELECT * FROM employee").then(results => {
-//         console.log("----------- EMPLOYEES ----------- ")
-//         console.table(results)
-//         // console.log("----------- EMPLOYEES ----------- ")
-//         return showAllEmployees();
-//     })
-// }
-
-// const start = () => {
-//     inquirer.prompt(startQuestions)
-//         .then(response => {
-//             console.log("the user chose", response)
-
-//             // based on user choice, we'll ask additional questions
-//             switch (response.questions) {
-//                 case "Show All Departments":
-//                     return showDepartments();
-//                 case "Show All Roles":
-//                     return showRoles();
-//                 case "Show All Employees":
-//                     return showAllEmployees();
-//                 case "Add A Department":
-//                     return addDepartment();
-//                 case "Add A Role":
-//                     return addRole();
-//                 case "Add An Employee":
-//                     return addEmployee();
-//                 case "Update An Employee's Role":
-//                     return updateEmployeeRole();
-//             }
-//         })
-// }
-
 const start = () => {
     inquirer.prompt(startQuestions)
         .then(response => {
@@ -171,22 +53,8 @@ const start = () => {
         })
 }
 
-
-// const showDepartments = () => {
-//     // calls to db, and show all employees
-//     db.query("SELECT * FROM department").then(results => {
-//         console.log("----------- DEPARTMENTS ----------- ")
-//         console.table(results);
-//         // console.log("----------- EMPLOYEES ----------- ")
-//         setTimeout(start, 3000);
-//     });
-// }
-
 const init = () => {
     start();
 }
-
-
-
 
 init()
